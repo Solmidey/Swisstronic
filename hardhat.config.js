9c7a0c4d7f69b81031ee6f643b1169a67a4826e2
Solmidey <olorunfemiosolomon@gmail.com>
2024-07-26 15:24:59 +0100

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [""], //Your private key starting with "0x"
+      accounts: ["0xee2ab869f643ba6fbe014ce55e5d43b098e0b8e2e8fce82ab6f6f299cd228fa8"], //Your private key starting with "0x"
     },
   },
 };
